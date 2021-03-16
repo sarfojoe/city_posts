66d6d2ec30cbf8c54b420930ae2de4211761f13b
sarfojoe <60213446+sarfojoe@users.noreply.github.com>
2021-03-16 22:02:00 +0200

    users updated

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -5,7 +5,7 @@ const Users = () => {
     const USERS = [{
         id: "u1",
         name: "Joseph Boadi",
-        image: "https://unsplash.com/photos/OD9EOzfSOh0",
+        image: "https://orthostudio.ca/wp-content/uploads/2016/11/image-3.jpg",
         place: 3,
     }, ];
     return <UsersList items = { USERS }
