bbb58443e0cb9aeba65d7a4f31abca59ce24532e
sarfojoe <60213446+sarfojoe@users.noreply.github.com>
2021-03-16 14:02:21 +0200

    userlist added

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,11 +1,15 @@
 import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-  return (
-    <div>
-      <h2> Users works </h2>
-    </div>
-  );
+    const USERS = [{
+        id: "u1",
+        name: "Joseph Boadi",
+        image: "https://unsplash.com/photos/OD9EOzfSOh0",
+        place: 3,
+    }, ];
+    return <UsersList items = { USERS }
+    />;
 };
 
 export default Users;
